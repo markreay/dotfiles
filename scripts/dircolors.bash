03e2238a11c773070f0256a462354c526ec5ebca
Mark Reay <markreay@gmail.com>
2021-07-07 15:09:32 -0700

    Cleaned up dircolors for MacOS

--- scripts/dircolors.bash
+++ scripts/dircolors.bash
@@ -1,8 +1,6 @@
 TITLE Applying dircolors . . .
 
-eval `dircolors $dir/.dir_colors`
-
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+if which dircolors > /dev/null
+then
+    eval `dircolors $dir/.dir_colors`
+fi
