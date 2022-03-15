8ee6838045c10b9faa070cef52db9823447aff4b
Mark Reay <markreay@gmail.com>
2022-03-15 11:52:07 -0700

    Improved ssh-agent handling

--- scripts/ssh-agent.bash
+++ scripts/ssh-agent.bash
@@ -1,29 +1,32 @@
 ############################################
 # Start ssh-agent
-# adapted from http://mah.everybody.org/docs/ssh
+# adapted from https://stackoverflow.com/questions/40549332/how-to-check-if-ssh-agent-is-already-running-in-bash
 
 TITLE Enabling ssh-agent . . .
 
 SSH_ENV="$HOME/.ssh/environment"
 
-function start_ssh_agent {
-    INFO "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    INFO succeeded
-    chmod 600 "${SSH_ENV}"
+function check-ssh-agent {
+  ssh-add -l &> /dev/null
+  if [[ "$?" == 2 ]]; then
+    # Try to connect to running ssh-agent
+    if [ -f "${SSH_ENV}" ]; then
+      . "${SSH_ENV}" > /dev/null
+    fi
+  fi
+
+  ssh-add -l &> /dev/null
+  if [[ "$?" == 2 ]]; then
+    # Try to restart ssh-agent
+    (umask 066; /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}")
     . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
+  fi
+
+  ssh-add -l &> /dev/null
+  if [[ "$?" == 1 ]]; then
+    ssh-add
+  fi
+  ssh-add -l
 }
 
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    
-    if [ "$(basename $(ps -o command -p ${SSH_AGENT_PID} | tail -1 | cut -f1 -d' ') 2>/dev/null)" != "ssh-agent" ]; then
-        WARNING "ssh-agent not running on ${SSH_AGENT_PID} as expected . . . restarting"
-        start_ssh_agent;
-    else
-        INFO "SSH agent already running on PID ${SSH_AGENT_PID}"
-    fi
-else
-    start_ssh_agent;
-fi
+check-ssh-agent
