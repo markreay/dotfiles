4a2aeec2e1aaa679479c9f034b04ad4198f108eb
Mark Reay <markreay@gmail.com>
2023-02-10 14:06:33 -0800

    Removed global git config check for codespaces

--- scripts/git.bash
+++ scripts/git.bash
@@ -4,14 +4,20 @@ TITLE Setting up git . . .
 # Set default editor to VIM
 export EDITOR=vim
 
-if [[ ! $(git config --global --get user.name) ]]
+if [[ ! $CODESPACES ]]
+# Codespaces doesn't support global configuration - configuration is per repo
 then
-    WARNING No name in git config
-    FIX git config --global user.name YOUR NAME
-fi
 
-if [[ ! $(git config --global --get user.email) ]]
-then
-    WARNING No name in git config
-    FIX git config --global user.email YOUR@EMAIL.ADDRESS
+    if [[ ! $(git config --global --get user.name) ]]
+    then
+        WARNING No name in git config
+        FIX git config --global user.name YOUR NAME
+    fi
+
+    if [[ ! $(git config --global --get user.email) ]]
+    then
+        WARNING No name in git config
+        FIX git config --global user.email YOUR@EMAIL.ADDRESS
+    fi
+
 fi
