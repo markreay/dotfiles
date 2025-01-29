932fcf4d139d5cd38fe3e002da7ffb3b0315ade9
Mark Reay <markreay@gmail.com>
2025-01-28 16:34:07 -0800

    pyenv: load environment variables from .env file if present

--- scripts/python.bash
+++ scripts/python.bash
@@ -3,5 +3,9 @@ function pyenv()
     if VENV=$(python3 -m pipenv --venv)
     then
         source $VENV/bin/activate
+        if [ -f $VENV/.env ]
+        then
+            source $VENV/.env
+        fi
     fi
 }
