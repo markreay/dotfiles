faa23b070c05ef4720304688c42dd8363310afe6
Mark Reay <markreay@gmail.com>
2025-02-05 15:25:48 -0800

    password-store: enhance status messages for local changes and new secrets

--- scripts/password-store.bash
+++ scripts/password-store.bash
@@ -5,14 +5,21 @@ TITLE Checking password store . . .
         pass git status --porcelain -b --ignore-submodules
     }
 
-    if (pass --help > /dev/null 2> /dev/null); then
+    if (pass --help >/dev/null 2>/dev/null); then
+
+        absPath=$(pass git rev-parse --show-toplevel)
+        nicePath=${absPath/#$HOME/~}
+
         pass git fetch --all -q
-        if (pass-git-status | head -1 | grep -q '\[behind'); then
+
+        if (pass-git-status | head -1 | grep -q '\[ahead\s[0-9]*,\sbehind\s[0-9]*\]'); then
+            INFO "Local committed changes to push and new secrets available in $nicePath"
+            INFO "To update, (pass git pull) and (pass git push)"
+        elif (pass-git-status | head -1 | grep -q '\[behind'); then
             INFO "New secrets available!"
             INFO "To update, (pass git pull)"
-        fi
-        if (pass-git-status | head -1 | grep -q '\[ahead'); then
-            INFO "Local committed changes to push in $dirName"
+        elif (pass-git-status | head -1 | grep -q '\[ahead'); then
+            INFO "Local committed changes to push in $nicePath"
             INFO "To update, (pass git push)"
         fi
     fi
