049cb4be9751dfdc4bb3470e3614354cfb5ad3be
Mark Reay <markreay@gmail.com>
2025-05-22 18:17:19 -0700

    history.bash: enhance history file management with hostname-based directories

--- scripts/history.bash
+++ scripts/history.bash
@@ -5,9 +5,12 @@ TITLE Enabling command history . . .
 
 # History file is now .history/20YY-MM-DD_tty1
 # Inspired by https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
-mkdir -p ~/.history
-chmod 700 ~/.history
-HISTFILE="$HOME/.history/$(date +%Y-%m-%d)_$(tty | sed 's/\///g;s/^dev//g')"
+HOSTNAME=$(hostname)
+HOSTNAME_SHORT=$(hostname | cut -d '.' -f 1 | tr '[:upper:]' '[:lower:]')
+HISTPATH=$HOME/.history/$HOSTNAME_SHORT
+mkdir -p $HISTPATH
+chmod 700 $HISTPATH
+HISTFILE="$HISTPATH/$(date +%Y-%m-%d)_$(tty | sed 's/\///g;s/^dev//g')"
 
 # Include timestamps in history
 HISTTIMEFORMAT="%H:%M:%S "
@@ -27,7 +30,10 @@ HISTFILESIZE=200000
 _bash_history_append() {
     builtin history -a
 }
-PROMPT_COMMAND="_bash_history_append; $PROMPT_COMMAND"
+
+if [[ "$PROMPT_COMMAND" != *"_bash_history_append"* ]]; then
+    PROMPT_COMMAND="_bash_history_append; $PROMPT_COMMAND"
+fi
 
 function hist() {
     search=$1
