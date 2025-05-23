1798e51f735d034ad8492b36a335e192527d0b85
Mark Reay <markreay@gmail.com>
2025-05-22 19:10:33 -0700

    add pass-sync function for streamlined password store updates

--- scripts/password-store.bash
+++ scripts/password-store.bash
@@ -5,6 +5,11 @@ TITLE Checking password store . . .
         pass git status --porcelain -b --ignore-submodules
     }
 
+    pass-sync() {
+        pass git pull --rebase
+        pass git push
+    }
+
     if (pass --help >/dev/null 2>/dev/null); then
 
         absPath=$(pass git rev-parse --show-toplevel)
@@ -14,7 +19,7 @@ TITLE Checking password store . . .
 
         if (pass-git-status | head -1 | grep -q '\[ahead\s[0-9]*,\sbehind\s[0-9]*\]'); then
             INFO "Local committed changes to push and new secrets available in $nicePath"
-            INFO "To update, (pass git pull) and (pass git push)"
+            INFO "To update, (pass-sync)"
         elif (pass-git-status | head -1 | grep -q '\[behind'); then
             INFO "New secrets available!"
             INFO "To update, (pass git pull)"
