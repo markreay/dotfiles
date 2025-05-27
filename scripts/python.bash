083be60c890e5aea5b63f2ce7951e35bf59db035
Mark Reay <markreay@gmail.com>
2025-05-27 13:59:46 -0700

    enhance pyenv function: improve virtual environment detection and add informative output

--- scripts/python.bash
+++ scripts/python.bash
@@ -1,11 +1,25 @@
 function pyenv()
 { 
-    if VENV=$(python3 -m pipenv --venv)
+   # if pipenv exists, use that to get VENV, else
+   # use python3 -m pipenv --venv
+
+    if command -v pipenv &> /dev/null
     then
-        source $VENV/bin/activate
-        if [ -f $VENV/.env ]
-        then
-            source $VENV/.env
-        fi
+        VENV=$(pipenv --venv)
+    else
+        VENV=$(python3 -m pipenv --venv)
     fi
+    if [ -z "$VENV" ]
+    then
+        echo "No virtual environment found."
+        return 1
+    fi
+    source $VENV/bin/activate
+    if [ -f $VENV/.env ]
+    then
+        source $VENV/.env
+    fi
+
+    echo "Activated virtual environment: $VENV"
+    echo "Python version: $(python --version)"
 }
