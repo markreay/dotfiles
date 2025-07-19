a6e5ec98539db785890a2e1bda80f8bf816e696d
Mark Reay <markreay@gmail.com>
2025-07-19 12:57:27 -0700

    add default branch configuration to git setup script

--- scripts/git.bash
+++ scripts/git.bash
@@ -8,6 +8,18 @@ if [[ ! $CODESPACES ]]
 # Codespaces doesn't support global configuration - configuration is per repo
 then
 
+    DEFAULT_GIT_BRANCH=main
+
+    if [[ ! $(git config --global --get init.defaultBranch)  ]]
+    then
+        WARNING No default branch in git config
+        FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
+    elif [[ ${GIT_DEFAULT_BRANCH:-$DEFAULT_GIT_BRANCH} != $(git config --global --get init.defaultBranch) ]]
+    then
+        WARNING Default branch in git config is not $DEFAULT_GIT_BRANCH
+        FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
+    fi
+
     if [[ ! $(git config --global --get user.name) ]]
     then
         WARNING No name in git config
