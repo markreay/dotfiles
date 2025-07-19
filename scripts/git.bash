f1494235e7751838b72ab05089970582c641efdd
Mark Reay <markreay@gmail.com>
2025-07-19 12:58:50 -0700

    refactor git setup script: moved code into subshell

--- scripts/git.bash
+++ scripts/git.bash
@@ -4,32 +4,34 @@ TITLE Setting up git . . .
 # Set default editor to VIM
 export EDITOR=vim
 
-if [[ ! $CODESPACES ]]
-# Codespaces doesn't support global configuration - configuration is per repo
-then
-
-    DEFAULT_GIT_BRANCH=main
-
-    if [[ ! $(git config --global --get init.defaultBranch)  ]]
+(
+    if [[ ! $CODESPACES ]]
+    # Codespaces doesn't support global configuration - configuration is per repo
     then
-        WARNING No default branch in git config
-        FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
-    elif [[ ${GIT_DEFAULT_BRANCH:-$DEFAULT_GIT_BRANCH} != $(git config --global --get init.defaultBranch) ]]
-    then
-        WARNING Default branch in git config is not $DEFAULT_GIT_BRANCH
-        FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
+
+        DEFAULT_GIT_BRANCH=main
+
+        if [[ ! $(git config --global --get init.defaultBranch)  ]]
+        then
+            WARNING No default branch in git config
+            FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
+        elif [[ ${GIT_DEFAULT_BRANCH:-$DEFAULT_GIT_BRANCH} != $(git config --global --get init.defaultBranch) ]]
+        then
+            WARNING Default branch in git config is not $DEFAULT_GIT_BRANCH
+            FIX git config --global init.defaultBranch $DEFAULT_GIT_BRANCH
+        fi
+
+        if [[ ! $(git config --global --get user.name) ]]
+        then
+            WARNING No name in git config
+            FIX git config --global user.name YOUR NAME
+        fi
+
+        if [[ ! $(git config --global --get user.email) ]]
+        then
+            WARNING No name in git config
+            FIX git config --global user.email YOUR@EMAIL.ADDRESS
+        fi
+
     fi
-
-    if [[ ! $(git config --global --get user.name) ]]
-    then
-        WARNING No name in git config
-        FIX git config --global user.name YOUR NAME
-    fi
-
-    if [[ ! $(git config --global --get user.email) ]]
-    then
-        WARNING No name in git config
-        FIX git config --global user.email YOUR@EMAIL.ADDRESS
-    fi
-
-fi
+)
