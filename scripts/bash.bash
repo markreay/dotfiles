5722a78b20325dd8e75c4d12a4d2c43668ad9dc8
Mark Reay <markreay@gmail.com>
2025-09-21 12:36:15 -0700

    feat(bash.bash): enhance Homebrew Bash installation instructions for macOS

--- scripts/bash.bash
+++ scripts/bash.bash
@@ -7,9 +7,20 @@ if (( BASH_VERSINFO[0] < _min_bash_major )); then
     echo "    Your dotfiles require >= $_min_bash_major"
     echo "    Current shell: $SHELL"
     if is_mac_os; then
+        HOMEBREW_BASH="/opt/homebrew/bin/bash"
         echo "    macOS ships with Bash 3.2 due to licensing issues."
-        echo "    You can install a modern Bash via Homebrew."
-        echo "    👉 Run: brew install bash && chsh -s /opt/homebrew/bin/bash"
+        # check to see if installed
+        if [ ! -x "$HOMEBREW_BASH" ]; then
+            echo "    Homebrew Bash not found at $HOMEBREW_BASH"
+            echo "    You can install a modern Bash via Homebrew."
+            echo "    👉 Run: brew install bash"
+        fi
+        if ! grep -q "/opt/homebrew/bin/bash" /etc/shells; then
+            echo "    Add the new Bash to /etc/shells:"
+            echo "    👉 Run: echo /opt/homebrew/bin/bash | sudo tee -a /etc/shells"
+        fi
+        echo "    Then change your default shell:"
+        echo "    👉 Run: chsh -s /opt/homebrew/bin/bash"
     elif is_wsl; then
         echo "    Please upgrade your WSL distribution to get a modern Bash."
         echo "    👉 Run: wsl --update"
