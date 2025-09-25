2302501519442f05b330348f6c86ee8a83cc8568
Mark Reay <mark.reay@deque.com>
2025-09-25 16:25:17 -0700

    feat(alias.bash): remove unused axe function to streamline alias configuration

--- scripts/alias.bash
+++ scripts/alias.bash
@@ -4,13 +4,6 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-function axe() {
-    (
-        cd ~/Downloads || return
-        cmd.exe /c axe $*
-    )
-}
-
 if [[ -f ~/local/alias.bash ]]; then
     . ~/local/alias.bash
 fi
