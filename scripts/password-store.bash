716852b449a84925308433d3cdb1acbddf095b17
Mark Reay <mark.reay@deque.com>
2025-10-07 11:32:41 -0700

    feat(password-store): add check for remote repository existence before fetching updates

--- scripts/password-store.bash
+++ scripts/password-store.bash
@@ -15,6 +15,11 @@ TITLE Checking password store . . .
             (
                 pass git fetch --all -q
                 
+                has_remote=$(pass git rev-parse --abbrev-ref @{u} 2>/dev/null)
+                if [ -z "$has_remote" ]; then
+                    echo "🔐 Password Store: No remote repository found."
+                    return
+                fi
                 local=$(pass git rev-parse @)
                 remote=$(pass git rev-parse @{u})
                 base=$(pass git merge-base @ @{u})
@@ -22,23 +27,23 @@ TITLE Checking password store . . .
                 if [ $local != $remote ] && [ $local = $base ]; then
                     echo "🔐 Password Store: Updates available."
                 fi
+
+                absPath=$(pass git rev-parse --show-toplevel)
+                nicePath=${absPath/#$HOME/~}
+
+                pass git fetch --all -q
+
+                if (pass-git-status | head -1 | grep -q '\[ahead\s[0-9]*,\sbehind\s[0-9]*\]'); then
+                    INFO "Local committed changes to push and new secrets available in $nicePath"
+                    INFO "To update, (pass-sync)"
+                elif (pass-git-status | head -1 | grep -q '\[behind'); then
+                    INFO "New secrets available!"
+                    INFO "To update, (pass git pull)"
+                elif (pass-git-status | head -1 | grep -q '\[ahead'); then
+                    INFO "Local committed changes to push in $nicePath"
+                    INFO "To update, (pass git push)"
+                fi
             )
-
-            absPath=$(pass git rev-parse --show-toplevel)
-            nicePath=${absPath/#$HOME/~}
-
-            pass git fetch --all -q
-
-            if (pass-git-status | head -1 | grep -q '\[ahead\s[0-9]*,\sbehind\s[0-9]*\]'); then
-                INFO "Local committed changes to push and new secrets available in $nicePath"
-                INFO "To update, (pass-sync)"
-            elif (pass-git-status | head -1 | grep -q '\[behind'); then
-                INFO "New secrets available!"
-                INFO "To update, (pass git pull)"
-            elif (pass-git-status | head -1 | grep -q '\[ahead'); then
-                INFO "Local committed changes to push in $nicePath"
-                INFO "To update, (pass git push)"
-            fi
         fi
     fi
 )
